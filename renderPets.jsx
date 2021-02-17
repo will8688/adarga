9c79824e99d909358f4af6e9dd030bb332430fe7
Will morrison <willmorrisonwebdev@gmail.com>
2021-02-17 14:16:03 +0000

    Create renderPets.jsx

---
+++ renderPets.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const renderPets = ({ pets }) => {
+	if (pets && pets.length > 0) {
+  		const petTypes = pets.map(pet => <Span>{pet.type}</Span>)
+  		return (
+    			<list>
+      				{petTypes}
+    			</list>
+    		)
+  	}
+	return (
+  	<Error message={'No Pets'}>
+	)
+}
+export default renderPets
