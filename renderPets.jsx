734a0f5158cc0eb661d95ce061f0a57350f5d3b8
Will morrison <willmorrisonwebdev@gmail.com>
2021-02-17 14:24:32 +0000

    Update renderPets.jsx

--- renderPets.jsx
+++ renderPets.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const renderPets = ({ pets }) => {
 	if (pets && pets.length > 0) {
-  		const petTypes = pets.map(pet => <Span>{pet.type}</Span>)
-  		return (
-    			<list>
-      				{petTypes}
-    			</list>
-    		)
+		const petTypes = pets.map(pet => <Span>{pet.type}</Span>)
+		return (
+			<list>
+				{petTypes}
+			</list>
+		)
   	}
 	return (
-  	<Error message={'No Pets'}>
+  		<Error message={'No Pets'}>
 	)
 }
 export default renderPets
